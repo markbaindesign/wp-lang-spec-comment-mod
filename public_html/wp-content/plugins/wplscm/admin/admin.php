f4788ac177eff38ee5601a49e251dbdaf0c26a0f
markbain <markbaindesign@gmail.com>
2020-03-26 17:26:03 +0100

    Tidy formatting

--- public_html/wp-content/plugins/wplscm/admin/admin.php
+++ public_html/wp-content/plugins/wplscm/admin/admin.php
@@ -8,37 +8,38 @@ add_action('admin_init', 'baindesign_wplscm_settings_init');
 
 function baindesign_wplscm_settings_init()
 {
-	register_setting(
-		'discussion', 													// Existing options group
-		'baindesign_wplscm_email_settings'						// Entry in options table
-	);
+   register_setting(
+      'discussion',                                               // Existing options group
+      'baindesign_wplscm_email_settings'                          // Entry in options table
+   );
 
-	add_settings_section(
-		'baindesign-wplscm-email-section',						// Section ID
-		__('Multilingual Comment Moderation', '_bd_wplscm'),		// Section header
-		'baindesign_wplscm_settings_section_callback',		// Callback
-		'discussion' 													// Add section to Settings > Discussion
-	);
+   add_settings_section(
+      'baindesign-wplscm-email-section',                          // Section ID
+      __('Multilingual Comment Moderation', '_bd_wplscm'),        // Section header
+      'baindesign_wplscm_settings_section_callback',              // Callback
+      'discussion'                                                // Add section to 
+      // Settings > Discussion
+   );
 
-	/**
-	 * Add a setting field for each language
-	 */
+   /**
+    * Add a setting field for each language
+    */
 
-	$langs = icl_get_languages();
-	foreach ($langs as $lang) {
-		$code = $lang["code"];
-		$name = $lang["native_name"];
-		$name_t = $lang["translated_name"];
-		// error_log($code, 0);
-		add_settings_field(
-			'baindesign_wplscm_email_' . $code,						// ID
-			$name . ' (' . $name_t . ')', 			// Label
-			'baindesign_wplscm_email_field_render', // Function to display inputs
-			'discussion',													// Page to display on
-			'baindesign-wplscm-email-section',						// Section ID where to show field
-			array($code, $name)																// Passes to callback function
-		);
-	}
+   $langs = icl_get_languages();
+   foreach ($langs as $lang) {
+      $code = $lang["code"];
+      $name = $lang["native_name"];
+      $name_t = $lang["translated_name"];
+      // error_log($code, 0);
+      add_settings_field(
+         'baindesign_wplscm_email_' . $code,                  // ID
+         $name . ' (' . $name_t . ')',          // Label
+         'baindesign_wplscm_email_field_render', // Function to display inputs
+         'discussion',                                       // Page to display on
+         'baindesign-wplscm-email-section',                  // Section ID where to show field
+         array($code, $name)                                                // Passes to callback function
+      );
+   }
 }
 
 /**
@@ -47,17 +48,17 @@ function baindesign_wplscm_settings_init()
 
 function baindesign_wplscm_email_field_render(array $args)
 {
-	// error_log("Code: " . $args[1], 0);
+   // error_log("Code: " . $args[1], 0);
 
-	$options = get_option('baindesign_wplscm_email_settings');
-	$input_name = 'baindesign_wplscm_email_settings[baindesign_wplscm_email_' . $args[0] . ']';
-	$input_value_option = 'baindesign_wplscm_email_' . $args[0];
-	$input_value = $options[$input_value_option];
-	$input_placeholder = $args[0] . '@example.com';
-	$input_id = 'email-' . $args[0];
+   $options = get_option('baindesign_wplscm_email_settings');
+   $input_name = 'baindesign_wplscm_email_settings[baindesign_wplscm_email_' . $args[0] . ']';
+   $input_value_option = 'baindesign_wplscm_email_' . $args[0];
+   $input_value = $options[$input_value_option];
+   $input_placeholder = $args[0] . '@example.com';
+   $input_id = 'email-' . $args[0];
 
-	// Render
-	echo '<input type="email" id="' . $input_id . '" name="' . $input_name . '" value="' . $input_value . '" placeholder="' . $input_placeholder . '">';
+   // Render
+   echo '<input type="email" id="' . $input_id . '" name="' . $input_name . '" value="' . $input_value . '" placeholder="' . $input_placeholder . '">';
 }
 
 /**
@@ -65,6 +66,6 @@ function baindesign_wplscm_email_field_render(array $args)
  */
 function baindesign_wplscm_settings_section_callback()
 {
-	$default_email = get_option('admin_email');
-	printf( __('For each of the following registered languages, you may add a specific Administration Email Address for comment moderation.<br />This email will be used for comments on posts in that language, instead of the default Administration Email Address (%s).', '_bd_wplscm'),$default_email);
+   $default_email = get_option('admin_email');
+   printf(__('For each of the following registered languages, you may add a specific Administration Email Address for comment moderation.<br />This email will be used for comments on posts in that language, instead of the default Administration Email Address (%s).', '_bd_wplscm'), $default_email);
 }
--- public_html/wp-content/plugins/wplscm/wplscm.php
+++ public_html/wp-content/plugins/wplscm/wplscm.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Language-specific Comment Moderation
  * Plugin URI: https://bain.design/wplscm
@@ -12,104 +13,104 @@
  * Plugin Slug: wplscm
  */
 
-function baindesign_wplscm_plugin_init() {
+function baindesign_wplscm_plugin_init()
+{
 
-	include( plugin_dir_path( __FILE__ ) . 'admin/admin.php');
-	
-	if( class_exists( 'SitePress' ) ) {
+   include(plugin_dir_path(__FILE__) . 'admin/admin.php');
 
-		/**
-		 * Get post language details
-		 */
-		function baindesign_wplscm_get_post_language($post_id)
-		{
-			$post_language_details = apply_filters('wpml_post_language_details', NULL, $post_id);
-			return $post_language_details;
-		}
+   if (class_exists('SitePress')) {
 
-		/**
-		 * Get the moderation email by language
-		 * 
-		 * As an input, this function requires the WPML language code. 
-		 * 
-		 * This function outputs an email address. 
-		 * 
-		 */
-		function baindesign_wplscm_get_local_moderation_email($lang_code)
-		{			
-			if( !$lang_code ) {
-				return;
-			}
-			$emails = get_option('baindesign_wplscm_email_settings');
-			if( !$emails ) {
-				return; // No email set
-			}
-			// Email found
-			$email = $emails['baindesign_wplscm_email_' . $lang_code];
-			return $email;
-		}
+      /**
+       * Get post language details
+       */
+      function baindesign_wplscm_get_post_language($post_id)
+      {
+         $post_language_details = apply_filters('wpml_post_language_details', NULL, $post_id);
+         return $post_language_details;
+      }
 
-		/**
-		 * Filter the comment moderation email
-		 */
+      /**
+       * Get the moderation email by language
+       * 
+       * As an input, this function requires the WPML language code. 
+       * 
+       * This function outputs an email address. 
+       * 
+       */
+      function baindesign_wplscm_get_local_moderation_email($lang_code)
+      {
+         if (!$lang_code) {
+            return;
+         }
+         $emails = get_option('baindesign_wplscm_email_settings');
+         if (!$emails) {
+            return; // No email set
+         }
+         // Email found
+         $email = $emails['baindesign_wplscm_email_' . $lang_code];
+         return $email;
+      }
 
-		function baindesign_wplscm_comment_moderation_recipients($emails, $comment_id)
-		{
-			// vars
-			$comment = get_comment($comment_id);
-			$post = get_post( $comment->comment_post_ID );
-			$post_id = $post->ID;
+      /**
+       * Filter the comment moderation email
+       */
 
-			// Get the Admin email from Settings > General
-			// This will be used if no specific email set for language. 
-			$default_email = get_option('admin_email');
+      function baindesign_wplscm_comment_moderation_recipients($emails, $comment_id)
+      {
+         // vars
+         $comment = get_comment($comment_id);
+         $post = get_post($comment->comment_post_ID);
+         $post_id = $post->ID;
 
-			if( is_wp_error( $post ) ) {
-				return; // Bail early
-			}
+         // Get the Admin email from Settings > General
+         // This will be used if no specific email set for language. 
+         $default_email = get_option('admin_email');
 
-			if( ! is_wp_error( $post_id ) ) {
+         if (is_wp_error($post)) {
+            return; // Bail early
+         }
 
-				// Get the post language
-				$lang_details = baindesign_wplscm_get_post_language($post_id);
+         if (!is_wp_error($post_id)) {
 
-				if( ! is_wp_error( $post_id ) ) {
-					$language_code = $lang_details['language_code'];
+            // Get the post language
+            $lang_details = baindesign_wplscm_get_post_language($post_id);
 
-					// Check if an email has been set for this language
-					$local_email = baindesign_wplscm_get_local_moderation_email($language_code);
-					
-					// Create an empty array for the email address
-					$emails = array();
-					if ( $local_email ){
-						// An email has been set for this lang. Add to array.
-						$emails[] = $local_email;
-					} else {
-						// No email set, add the default email to the array.
-						$emails[] = $default_email;
-					}
-					return $emails;
-				}
-			}
+            if (!is_wp_error($post_id)) {
+               $language_code = $lang_details['language_code'];
 
-		}
-		add_filter('comment_moderation_recipients', 'baindesign_wplscm_comment_moderation_recipients', 11, 2);
+               // Check if an email has been set for this language
+               $local_email = baindesign_wplscm_get_local_moderation_email($language_code);
 
-	} else {
+               // Create an empty array for the email address
+               $emails = array();
+               if ($local_email) {
+                  // An email has been set for this lang. Add to array.
+                  $emails[] = $local_email;
+               } else {
+                  // No email set, add the default email to the array.
+                  $emails[] = $default_email;
+               }
+               return $emails;
+            }
+         }
+      }
+      add_filter('comment_moderation_recipients', 'baindesign_wplscm_comment_moderation_recipients', 11, 2);
+   } else {
 
-		/**
-		 * Show an admin message to tell the user to install
-		 * WPML plugin. 
-		 */
-		function baindesign_wplscm_admin_warning() {
-			ob_start(); ?>
-			<div class="error">
-				<?php _e('<p><strong>Error</strong>: You must activate the WPML plugin for the Language-specific Comment Moderation to work!</p>', '_bd_wplscm' ); ?>
-			</div>
-			<?php
-			echo ob_get_clean();
-		}
-		add_action('admin_notices', 'baindesign_wplscm_admin_warning');
-	}
+      /**
+       * Show an admin message to tell the user to install
+       * WPML plugin. 
+       */
+      function baindesign_wplscm_admin_warning()
+      {
+         ob_start(); ?>
+         <div class="error">
+            <?php _e('<p><strong>Error</strong>: You must activate the WPML plugin for the Language-specific Comment Moderation to work!</p>', '_bd_wplscm'); ?>
+         </div>
+<?php
+         echo ob_get_clean();
+      }
+      add_action('admin_notices', 'baindesign_wplscm_admin_warning');
+   }
 }
-add_action( 'plugins_loaded', 'baindesign_wplscm_plugin_init' );
+add_action('plugins_loaded', 'baindesign_wplscm_plugin_init');
